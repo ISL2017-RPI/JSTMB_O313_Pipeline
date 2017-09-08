b95eb97a32c967c63ec5146f4fb500b3038ba4c6
ISL2017-RPI <31634008+ISL2017-RPI@users.noreply.github.com>
2017-09-08 15:46:00 -0400

    Update O313_JSTMB_wrapper.py

--- O313_JSTMB_wrapper.py
+++ O313_JSTMB_wrapper.py
@@ -2,7 +2,7 @@ import sys
 import JSTMB_O313
 import numpy as np
 
-def JSTMB(data_file, target_file):
+def JSTMB(data_file = 'trainData.csv', target_file = 'trainargets.csv'):
     my_JMI = JSTMB_O313.initialize()
     feat = my_JMI.JSTMB_primitive_O313(data_file, target_file)
     return feat
