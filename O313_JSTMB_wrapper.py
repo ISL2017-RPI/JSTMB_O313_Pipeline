6d1d9be00a4ccc53b8d046cf4b96309e2b62a41c
ISL2017-RPI <31634008+ISL2017-RPI@users.noreply.github.com>
2017-09-08 15:07:01 -0400

    Update O313_JSTMB_wrapper.py

--- O313_JSTMB_wrapper.py
+++ O313_JSTMB_wrapper.py
@@ -4,14 +4,13 @@ import numpy as np
 
 def JSTMB(data_file, target_file, hm_feat):
     my_JMI = JSTMB_O313.initialize()
-    feat = my_JMI.JSTMB_primitive_O313(data_file, target_file, hm_feat)
+    feat = my_JMI.JSTMB_primitive_O313(data_file, target_file)
     return feat
 
 
 if __name__ == "__main__":
     data = sys.argv[1]
     target = sys.argv[2]
-    hm_feat = sys.argv[3]
-    selected_feature = np.array(JSTMB(data, target, hm_feat), dtype=np.int16)
+    selected_feature = np.array(JSTMB(data, target), dtype=np.int16)
     np.savetxt('Features_O313_JSTMB.csv', selected_feature, delimiter=',')
     print selected_feature
